8739fe19e8dc2ffe75ad87b8898c23a76dbb304d
zach lewis <zlewis@clarityinnovates.com>
2022-07-20 13:57:19 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import soon from "./comingsoon.png"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={soon} alt="soon"  className="App-logo"/>
+        <h1>Yes coming soon</h1>
+    </div>
+  );
+}
+
+export default App;
